d83185a6f22defd6e125c9bcbde5dd072496c2c6
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-02-09 14:38:55 -0500

    Added Tag english lang

---
+++ lua/terrortown/lang/english/more_mg.lua
@@ -0,0 +1,9 @@
+L = LANG.GetLanguageTableReference("english")
+
+--Tag
+L["ttt2mg_tag_is_it"] = "{nick} is it!"
+L["ttt2mg_tag_tagged"] = "You tagged {nick}!"
+L["ttt2mg_tag_exploded"] = "{nick} ran out of time!"
+L["ttt2mg_tag_no_explode"] = "No one is out this time!"
+
+--Recruitment Drive
