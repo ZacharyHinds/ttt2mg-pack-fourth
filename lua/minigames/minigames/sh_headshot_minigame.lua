76aab72baae8659fd989b1eb1db26613e01600ec
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-02-09 14:53:08 -0500

    Created Headshots Only minigame

---
+++ lua/minigames/minigames/sh_headshot_minigame.lua
@@ -0,0 +1,41 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+--CONCEPT: Only headshots count
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Headshots Only!"
+    },
+    desc = {
+      English = ""
+    }
+  }
+end
+
+if SERVER then
+
+
+  function MINIGAME:OnActivation()
+    hook.Add("ScalePlayerDamage", "HeadshotMinigameScale", function(ply, hitgroup, dmginfo)
+      if hitgroup ~= HITGROUP_HEAD then
+        dmginfo:ScaleDamage(0)
+      end
+    end)
+  end
+
+  function MINIGAME:OnDeactivation()
+    hook.Remove("ScalePlayerDamage", "HeadshotMinigameScale")
+  end
+
+  -- function MINIGAME:IsSelectable()
+  --   return false
+  -- end
+end
