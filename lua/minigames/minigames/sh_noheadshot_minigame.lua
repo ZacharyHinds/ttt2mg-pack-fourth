54219b0a56b51ab9796405a63f6cce9bcaf7a646
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-02-14 22:05:22 -0500

    Created No Headshot minigame

---
+++ lua/minigames/minigames/sh_noheadshot_minigame.lua
@@ -0,0 +1,39 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+--Concept: Disable headshots
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "No Headshots!"
+    },
+    desc = {
+      English = "Hardhat Zone!"
+    }
+  }
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    hook.Add("ScalePlayerDamage", "NoHeadshotMinigame", function(ply, hitgroup, dmginfo)
+      if hitgroup == HITGROUP_HEAD then
+        dmginfo:ScaleDamage(0.01)
+      end
+    end)
+  end
+
+  function MINIGAME:OnDeactivation()
+    hook.Remove("ScalePlayerDamage", "NoHeadshotMinigame")
+  end
+  --
+  -- function MINIGAME:IsSelectable()
+  --   return false
+  -- end
+end
