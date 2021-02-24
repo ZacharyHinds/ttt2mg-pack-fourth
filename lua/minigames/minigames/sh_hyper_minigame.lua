8d9b035797db09adea694f9a83369f3c5839746a
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-02-24 09:58:39 -0500

    Created Hyperspeed minigame

---
+++ lua/minigames/minigames/sh_hyper_minigame.lua
@@ -0,0 +1,47 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+--Concept: Players receive occasional random inputs
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {
+  ttt2mg_hyper_speedmult = {
+    slider = true,
+    min = 1,
+    max = 100,
+    desc = "ttt2mg_hyper_speedmult (Def. 20)"
+  }
+}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      en = "Hyperspeed!"
+    },
+    desc = {
+      en = "Movement speed waaaaay up!!!"
+    }
+  }
+end
+
+if SERVER then
+  local ttt2mg_hyper_speedmult = CreateConVar("ttt2mg_hyper_speedmult", "20", {FCVAR_ARCHIVE}, "Speed multiplier")
+  function MINIGAME:OnActivation()
+    hook.Add("TTTPlayerSpeedModifier", "HyperSpeedMG", function(ply, _, _, speedMultiplierMod)
+      if GetRoundState() ~= ROUND_ACTIVE then hook.Remove("TTTPlayerSpeedModifier", "HyperSpeedMG") return end
+      if not IsValid(ply) or not ply:IsPlayer() or not ply:Alive() or ply:IsSpec() then return end
+      speedMultiplierMod[1] = speedMultiplierMod[1] * ttt2mg_hyper_speedmult:GetFloat()
+    end)
+  end
+
+  function MINIGAME:OnDeactivation()
+    hook.Remove("TTTPlayerSpeedModifier", "HyperSpeedMG")
+  end
+
+  -- function MINIGAME:IsSelectable()
+  --   return true
+  -- end
+end
