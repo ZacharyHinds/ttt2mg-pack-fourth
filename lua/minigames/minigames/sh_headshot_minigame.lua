ce4a5bae210f957d417926411ed020cc8efd3b1d
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-02-24 09:58:24 -0500

    Fix for No-Headshots + Headshots minigame

--- lua/minigames/minigames/sh_headshot_minigame.lua
+++ lua/minigames/minigames/sh_headshot_minigame.lua
@@ -7,7 +7,15 @@ end
 MINIGAME.author = "Wasted"
 MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
 
-MINIGAME.conVarData = {}
+MINIGAME.conVarData = {
+  ttt2mg_headshot_scale = {
+    slider = true,
+    decimal = 2,
+    min = 0,
+    max = 1,
+    desc = "ttt2mg_headshot_scale (Def. 0)"
+  }
+}
 
 if CLIENT then
   MINIGAME.lang = {
@@ -22,11 +30,18 @@ end
 
 if SERVER then
 
-
+  local ttt2mg_headshot_scale = CreateConVar("ttt2mg_headshot_scale", "0", {FCVAR_ARCHIVE}, "Damage Scaler for non-headshots")
   function MINIGAME:OnActivation()
+    local activeMgs = minigames.GetActiveList()
+    for i = 1, #activeMgs do
+      if activeMgs[i].name == "sh_headshot_minigame" then return end
+    end
     hook.Add("ScalePlayerDamage", "HeadshotMinigameScale", function(ply, hitgroup, dmginfo)
+      if GetRoundState() ~= ROUND_ACTIVE then hook.Remove("ScalePlayerDamage", "HeadshotMinigameScale") return end
+      local atk = dmginfo:GetAttacker()
+      if not atk or not IsValid(atk) or not atk:IsPlayer() then return end
       if hitgroup ~= HITGROUP_HEAD then
-        dmginfo:ScaleDamage(0)
+        dmginfo:ScaleDamage(ttt2mg_headshot_scale:GetFloat())
       end
     end)
   end
@@ -35,7 +50,10 @@ if SERVER then
     hook.Remove("ScalePlayerDamage", "HeadshotMinigameScale")
   end
 
-  -- function MINIGAME:IsSelectable()
-  --   return false
-  -- end
+  function MINIGAME:IsSelectable()
+    local activeMgs = minigames.GetActiveList()
+    for i = 1, #activeMgs do
+      if activeMgs[i].name == "sh_noheadshot_minigames" then return false end
+    end
+  end
 end
--- lua/minigames/minigames/sh_noheadshot_minigame.lua
+++ lua/minigames/minigames/sh_noheadshot_minigame.lua
@@ -7,7 +7,15 @@ end
 MINIGAME.author = "Wasted"
 MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
 
-MINIGAME.conVarData = {}
+MINIGAME.conVarData = {
+  ttt2mg_noheadshot_scale = {
+    slider = true,
+    decimal = 2,
+    min = 0,
+    max = 1,
+    desc = "ttt2mg_noheadshot_scale (Def. 0.01)"
+  }
+}
 
 if CLIENT then
   MINIGAME.lang = {
@@ -21,10 +29,18 @@ if CLIENT then
 end
 
 if SERVER then
+  local ttt2mg_noheadshot_scale = CreateConVar("ttt2mg_noheadshot_scale", "0.01", {FCVAR_ARCHIVE}, "Damage scaler for headshots")
   function MINIGAME:OnActivation()
+    local activeMgs = minigames.GetActiveList()
+    for i = 1, #activeMgs do
+      if activeMgs[i].name == "sh_headshot_minigame" then return end
+    end
     hook.Add("ScalePlayerDamage", "NoHeadshotMinigame", function(ply, hitgroup, dmginfo)
+      if GetRoundState() ~= ROUND_ACTIVE then hook.Remove("ScalePlayerDamage", "NoHeadshotMinigame") return end
+      local atk = dmginfo:GetAttacker()
+      if not atk or not IsValid(atk) or not atk:IsPlayer() then return end
       if hitgroup == HITGROUP_HEAD then
-        dmginfo:ScaleDamage(0.01)
+        dmginfo:ScaleDamage(ttt2mg_noheadshot_scale:GetFloat())
       end
     end)
   end
@@ -33,7 +49,10 @@ if SERVER then
     hook.Remove("ScalePlayerDamage", "NoHeadshotMinigame")
   end
   --
-  -- function MINIGAME:IsSelectable()
-  --   return false
-  -- end
+  function MINIGAME:IsSelectable()
+    local activeMgs = minigames.GetActiveList()
+    for i = 1, #activeMgs do
+      if activeMgs[i].name == "sh_headshot_minigame" then return false end
+    end
+  end
 end
