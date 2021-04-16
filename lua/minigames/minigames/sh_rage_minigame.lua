5c7c50c79ea87ba4a0367ed465e6d6283f83d49c
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-04-16 00:34:30 -0400

    Created Seething Rage

---
+++ lua/minigames/minigames/sh_rage_minigame.lua
@@ -0,0 +1,40 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      en = "Seething Rage"
+    },
+    desc = {
+      en = "Anger compels me"
+    }
+  }
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    local plys = util.GetFilteredPlayers(function(ply)
+      return ply:Alive() and ply:IsPlayer() and not ply:IsSpec() and ply:HasTeam(TEAM_INNOCENT) and not ply:IsDetective()
+    end)
+
+    for i = 1, #plys do
+      plys[i]:SetRole(ROLE_WRATH)
+    end
+    SendFullStateUpdate()
+  end
+
+  function MINIGAME:OnDeactivation()
+
+  end
+
+  function MINIGAME:IsSelectable()
+    return WRATH
+  end
+end
