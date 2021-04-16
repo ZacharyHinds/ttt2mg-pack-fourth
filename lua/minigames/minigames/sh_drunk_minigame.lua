6350fc7ab73c2152821e0fe4b9e86a669347bd50
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-04-16 18:46:01 -0400

    Created Stumbling Drunk

---
+++ lua/minigames/minigames/sh_drunk_minigame.lua
@@ -0,0 +1,39 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+--Concept:
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      en = "Stumbling Drunk"
+    },
+    desc = {
+      en = "Flipped controls"
+    }
+  }
+end
+
+if CLIENT then
+  function MINIGAME:OnActivation()
+    hook.Add("StartCommand", "StumblingMinigameControl", function(ply, ucmd)
+      ucmd:SetForwardMove(ucmd:GetForwardMove() * -1)
+      ucmd:SetSideMove(ucmd:GetSideMove() * -1)
+      ucmd:SetUpMove(ucmd:GetUpMove() * -1)
+    end)
+  end
+
+  function MINIGAME:OnDeactivation()
+    hook.Remove("StartCommand", "StumblingMinigameControl")
+  end
+
+  -- function MINIGAME:IsSelectable()
+  --   return false
+  -- end
+end
