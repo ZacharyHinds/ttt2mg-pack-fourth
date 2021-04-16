003f818190fa3160a850733985143eb765cd3c8f
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-04-16 17:25:03 -0400

    Created DUCK!

---
+++ lua/minigames/minigames/sh_duck_minigame.lua
@@ -0,0 +1,57 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      en = "DUCK!"
+    },
+    desc = {
+      en = ""
+    }
+  }
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    hook.Add("StartCommand", "DuckMinigameCommand", function(ply, ucmd)
+      if ucmd:KeyDown(IN_DUCK) then
+        ply.duckMinigame_isDucked = true
+      else
+        ply.duckMinigame_isDucked = false
+      end
+    end)
+
+    local delay = CurTime() + 0.5
+    hook.Add("Think", "DuckMinigameThink", function()
+      if delay > CurTime() then return end
+      local dmginfo = DamageInfo()
+      dmginfo:SetAttacker(game.GetWorld())
+      dmginfo:SetDamage(1)
+      local plys = util.GetAlivePlayers()
+      for i = 1, #plys do
+        local ply = plys[i]
+        if not ply.duckMinigame_isDucked then
+          ply:TakeDamageInfo(dmginfo)
+        end
+      end
+      delay = CurTime() + 0.5
+    end)
+
+  end
+
+  function MINIGAME:OnDeactivation()
+    hook.Remove("StartCommand", "DuckMinigameCommand")
+    hook.Remove("Think", "DuckMinigameThink")
+  end
+  --
+  -- function MINIGAME:IsSelectable()
+  --   return false
+  -- end
+end
