6b4b55617f4d66254d613fa2ac3e7a847d40d6e0
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-04-16 18:48:00 -0400

    Created Imposter

---
+++ lua/minigames/minigames/sh_imposter_minigame.lua
@@ -0,0 +1,40 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+--Concept: A Random player switches to the Doppelganger team
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      en = "Imposter"
+    },
+    desc = {
+      en = "They're among us!"
+    }
+  }
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    local plys = util.GetAlivePlayers()
+    local ply = plys[math.random(#plys)]
+    ply:UpdateTeam(TEAM_DOPPELGANGER)
+    ply:SetNWBool("isDormantDoppelganger", true)
+  end
+
+  function MINIGAME:OnDeactivation()
+
+  end
+
+  function MINIGAME:IsSelectable()
+    if not DOPPELGANGER then
+      return false
+    end
+  end
+end
